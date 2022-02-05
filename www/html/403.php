35c4d6383da4c0aca8c5a104ca75b020bdc96108
Codername47 <Codername47@yandex.ru>
2022-02-05 14:53:43 +0300

    final

---
+++ www/html/403.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2>403</h2>";
+echo "<hr>";
+echo "<img src='ban.png'>";
+echo "<div>Доступ запрещён, анон!</div>";
---
+++ www/html/404.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2>404</h2>";
+echo "<hr>";
+echo "<img src='dv.jpg'>";
+echo "<div>Увы, страница не найдена</div>";
